de19702854ac3941d8b88ec2f7fe7fb6e34f882b
alexandershubin <shubin8@mail.ru>
2020-01-10 14:09:14 +0300

    add list

--- source/js/project.js
+++ source/js/project.js
@@ -3,18 +3,34 @@ window.addEventListener('DOMContentLoaded', function () {
 
   const listGallery = document.querySelector('.portfolio__list');
 
-  const gallery = [
-    {category: `graphics, web, marketing`, img: `img.png`},
-    {category: `graphics, web, marketing`, img: `img_2.png`},
-    {category: `graphics, web, illustrator`, img: `img_3.png`},
-    {category: `graphics, photoshop`, img: `img_4.png`},
-    {category: `graphics, marketing`, img: `img_5.png`}
+  const gallery = [{
+      category: `graphics, web, marketing`,
+      img: `img.png`
+    },
+    {
+      category: `graphics, web, marketing`,
+      img: `img_2.png`
+    },
+    {
+      category: `web, illustrator`,
+      img: `img_3.png`
+    },
+    {
+      category: `graphics, photoshop`,
+      img: `img_4.png`
+    },
+    {
+      category: `photoshop, graphics`,
+      img: `img_5.png`
+    }
   ];
 
   const filterButtons = () => {
     const btns = document.querySelectorAll(`.portfolio__buttons button`);
-    btns.forEach(item => {
-      item.addEventListener(`click`, () => {
+    btns.forEach((item) => {
+      item.addEventListener(`click`, (e) => {
+        btns.forEach(item => item.classList.remove('active'))
+        item.classList.add('active');
         const category = item.innerHTML.toLowerCase();
         renderGallery(category);
       })
