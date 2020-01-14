84aea485cc158490748f3049716dde8fe91ece85
alexandershubin <shubin8@mail.ru>
2020-01-14 18:24:20 +0300

    change menu js

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,51 +1,36 @@
+'use strict';
 window.addEventListener('DOMContentLoaded', function () {
-  'use strict';
+  const addOpenedMenuListener = () => {
+    const svgButton = document.querySelector('.ham');
+    const overlay = document.querySelector('.overlay');
+    const siteMenu = document.querySelector('.menu__list');
 
-  let menuButton = document.querySelector(".menu__button"),
-    menuList = document.querySelector(".menu__list"),
-    menuItem = document.querySelectorAll(".menu__item"),
-    overlay = document.querySelector(".overlay"),
-    body = document.querySelector("body");
+    let isActive = false;
 
-  // прописываем условия показа блока
-  function showBorder(b) {
-    if (menuItem[b].classList.contains('item-disable')) {
-      menuItem[b].classList.remove('item-disable');
-      menuItem[b].classList.add('item-active');
-    }
-  }
+    const toggleMenu = () => {
+      svgButton.classList.toggle('active');
+      overlay.classList.toggle('overlay-modal');
+      siteMenu.classList.toggle('active');
+      isActive = !isActive;
+    };
 
-  //скрываем все стили
-  function hideBorder() {
-    for (let i = 0; i < menuItem.length; i++) {
-      menuItem[i].classList.remove("item-active");
-      menuItem[i].classList.add("item-disable");
-    }
-  }
+    const addEscOnButton = () => {
+      window.addEventListener('keydown', (e) => {
+        if (e.keyCode == 27 && isActive) {
+          toggleMenu();
+        }
+      })
+    };
+    addEscOnButton();
 
-
-  for (let i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener("click", function () {
-      hideBorder();
-      showBorder(i);
+    window.addEventListener('click', (e) => {
+      const {target} = e;
+      console.log(target);
+      if (target.matches('.ham') || target.matches('.overlay')) {
+        toggleMenu();
+      }
     });
-  }
-
-  menuButton.addEventListener("click", function () {
-    if (menuButton.classList.contains("button-closed") && menuList.classList.contains("menu-closed")) {
-      menuButton.classList.remove("button-closed");
-      menuButton.classList.add("button-opened");
-      menuList.classList.remove("menu-closed");
-      menuList.classList.add("menu-opened");
-      overlay.classList.add("overlay-modal");
-      body.style.overflow = "hidden";
-    } else {
-      menuButton.classList.remove("button-opened");
-      menuButton.classList.add("button-closed");
-      menuList.classList.remove("menu-opened");
-      menuList.classList.add("menu-closed");
-      overlay.classList.remove("overlay-modal");
-      body.style.overflow = '';
-    }
-  });
+  };
+  addOpenedMenuListener();
 });
+
