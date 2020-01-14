79565b3890974346cce4d2872349ac311adfc0db
alexandershubin <shubin8@mail.ru>
2020-01-14 19:54:14 +0300

    add js on menu

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,32 +1,44 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', function () {
+  // функция для открытия и закрытия мменю
   const addOpenedMenuListener = () => {
     const svgButton = document.querySelector('.ham');
     const overlay = document.querySelector('.overlay');
     const siteMenu = document.querySelector('.menu__list');
-
+    const itemSite = document.querySelectorAll('.menu__item');
     let isActive = false;
+
+    // функция добавляет
+    itemSite.forEach(item => {
+      item.addEventListener('click', () => {
+        itemSite.forEach(el => el.classList.remove('active'));
+        item.classList.add('active');
+      })
+    });
 
     const toggleMenu = () => {
       svgButton.classList.toggle('active');
       overlay.classList.toggle('overlay-modal');
       siteMenu.classList.toggle('active');
+      document.body.classList.toggle('hide');
       isActive = !isActive;
     };
 
+    // добавляет кликна закрытие меню по кнопке Esc
     const addEscOnButton = () => {
       window.addEventListener('keydown', (e) => {
-        if (e.keyCode == 27 && isActive) {
+        const isEscKey = e.key === `Escape` || e.key === `Esc`;
+        if (isEscKey && isActive) {
           toggleMenu();
         }
       })
     };
     addEscOnButton();
 
+    // добавляет клик на закрытие меню за пределами меню
     window.addEventListener('click', (e) => {
       const {target} = e;
-      console.log(target);
-      if (target.matches('.ham') || target.matches('.overlay')) {
+      if (target.matches('.ham') || target.parentNode.matches('.ham') || target.matches('.overlay')) {
         toggleMenu();
       }
     });
